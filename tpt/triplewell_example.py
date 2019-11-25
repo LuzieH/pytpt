351b6c64f94687b16b022eb6e26f49fded789a4f
Luzie Helfmann <helfmann@zib.de>
2019-11-25 15:04:59 +0100

    #10 visualization effective current periodic and finite time case

--- tpt/triplewell_example.py
+++ tpt/triplewell_example.py
@@ -101,27 +101,6 @@ rate_f = well3_finite.transition_rate()
 
 
 
-###########################################################################
-#plotting
-#colormap e.g. cmap  = 'PuBuGn' or 'CMRmap' or 'viridis_r' or 'inferno_r'
-
-def plot_subplot_3well(data, datashape, extent, timeframe, size, v_min, v_max, title, subtitles=None):
-    fig, ax = plt.subplots(1, timeframe, sharex='col',
-                           sharey='row', figsize=size)
-    if timeframe == 1:
-        if np.isnan(data).all()==False: #if not all values are nan
-            plt.imshow(data.reshape(datashape), ax=ax, vmin=v_min, vmax=v_max, origin='lower', extent=extent)
-    else:
-        for i in range(timeframe):
-            if np.isnan(data[i,:]).all()==False: #if not all values are nan
-                ax[i].imshow(data[i,:].reshape(datashape), vmin=v_min, vmax=v_max, origin='lower', extent=extent)
-            if subtitles is not None:
-                ax[i].set_title(subtitles[i])  # , pad=0)
-    fig.suptitle(title)
-    fig.subplots_adjust(top=0.8)
-    return fig
-
-
 #############################################################################
 # plots  infinite-time, ergodic
 fig, ax = plt.subplots(1, 5, sharex='col',
@@ -163,6 +142,47 @@ ax[4].set_title(subtitles[4])
  
 fig.savefig(os.path.join(charts_path, 'triplewell_stat.png'), dpi=100)
 
+###########################################################################
+#plotting
+#colormap e.g. cmap  = 'PuBuGn' or 'CMRmap' or 'viridis_r' or 'inferno_r'
+
+
+
+def plot_subplot_3well_effcurrent(eff_vectors_unit, colors, xn, yn, background, datashape, extent, timeframe, size,title, subtitles=None):
+    fig, ax = plt.subplots(1, timeframe, sharex='col',
+                           sharey='row', figsize=size)
+    if timeframe == 1:
+        if np.isnan(eff_vectors_unit).all()==False: #if not all values are nan
+            plt.imshow(background.reshape(datashape), cmap='Greys', alpha=.4, ax=ax, origin='lower', extent=extent)
+            plt.quiver(xn,yn,list(eff_vectors_unit[:,0]),list(eff_vectors_unit[:,1]),colors,cmap='coolwarm', width=0.02, scale=25)    
+    else:
+        for i in range(timeframe):
+            if np.isnan(eff_vectors_unit[i,:,:]).all()==False: #if not all values are nan
+                ax[i].imshow(background.reshape(datashape), cmap='Greys', alpha=.4, origin='lower', extent=extent)
+                ax[i].quiver(xn,yn,list(eff_vectors_unit[i,:,0]),list(eff_vectors_unit[i,:,1]),colors[i],cmap='coolwarm', width=0.02, scale=25)             
+            if subtitles is not None:
+                ax[i].set_title(subtitles[i])  # , pad=0)
+    fig.suptitle(title)
+    fig.subplots_adjust(top=0.8)
+    return fig
+
+
+
+def plot_subplot_3well( data,datashape, extent, timeframe, size, v_min, v_max, title, subtitles=None):
+    fig, ax = plt.subplots(1, timeframe, sharex='col',
+                           sharey='row', figsize=size)
+    if timeframe == 1:
+        if np.isnan(data).all()==False: #if not all values are nan
+            plt.imshow(data.reshape(datashape), ax=ax, vmin=v_min, vmax=v_max, origin='lower', extent=extent)
+    else:
+        for i in range(timeframe):
+            if np.isnan(data[i,:]).all()==False: #if not all values are nan
+                ax[i].imshow(data[i,:].reshape(datashape), vmin=v_min, vmax=v_max, origin='lower', extent=extent)
+            if subtitles is not None:
+                ax[i].set_title(subtitles[i])  # , pad=0)
+    fig.suptitle(title)
+    fig.subplots_adjust(top=0.8)
+    return fig
 ######################################################## plots periodic
 
 subtitles_p = np.array(['m = ' + str(i) for i in np.arange(M)])
@@ -193,6 +213,26 @@ fig = plot_subplot_3well(data, (xdim,ydim), (interval[0,0],interval[0,1],interva
 fig.savefig(os.path.join(charts_path, 'triplewell_reac_dens_p.png'), dpi=100)
 
 
+#define AB sets
+densAB = np.zeros(dim_st)
+densAB[ind_A]=1
+densAB[ind_B]=1
+
+#calculation the effective vector for each state
+eff_vectors_p = np.zeros((M,dim_st, 2))
+eff_vectors_unit_p = np.zeros((M,dim_st, 2))
+colors_p = np.zeros((M,dim_st))
+for m in np.arange(M):
+    for i in np.arange(dim_st):
+        for j in np.arange(dim_st):
+            eff_vectors_p[m,i,0] += eff_current_p[m,i,j] *  (xn[j] - xn[i])  
+            eff_vectors_p[m,i,1] += eff_current_p[m,i,j] *  (yn[j] - yn[i])  
+        colors_p[m,i] = np.linalg.norm(eff_vectors_p[m,i,:])
+        if colors_p[m,i]>0:
+            eff_vectors_unit_p[m,i,:] = eff_vectors_p[m,i,:]/colors[m,i]
+            
+fig = plot_subplot_3well_effcurrent(eff_vectors_unit_p, colors_p, xn, yn, densAB,(xdim,ydim), (interval[0,0],interval[0,1],interval[1,0],interval[1,1]), M, (3*M,3),'Periodic $f^+_m$', subtitles=subtitles_p)
+fig.savefig(os.path.join(charts_path, 'triplewell_eff_p.png'), dpi=100)
 ######################################################## plots finite-time
 
 subtitles_f = np.array(['n = ' + str(i) for i in np.arange(N)])
@@ -221,3 +261,20 @@ v_min = np.nanmin(data)
 v_max = np.nanmax(data)
 fig = plot_subplot_3well(data, (xdim,ydim), (interval[0,0],interval[0,1],interval[1,0],interval[1,1]) , N, (3*N,3), v_min, v_max, 'Finite-time $\mu^\mathcal{AB}(n)$', subtitles = subtitles_f)
 fig.savefig(os.path.join(charts_path, 'triplewell_reac_dens_f.png'), dpi=100)
+
+#calculation the effective vector for each state
+eff_vectors_f = np.zeros((N,dim_st, 2))
+eff_vectors_unit_f = np.zeros((N,dim_st, 2))
+colors_f = np.zeros((N,dim_st))
+for n in np.arange(N):
+    for i in np.arange(dim_st):
+        for j in np.arange(dim_st):
+            #if np.isnan(eff_current_f[n,i,j])==False:
+            eff_vectors_f[n,i,0] += eff_current_f[n,i,j] *  (xn[j] - xn[i])  
+            eff_vectors_f[n,i,1] += eff_current_f[n,i,j] *  (yn[j] - yn[i])  
+        colors_f[n,i] = np.linalg.norm(eff_vectors_f[n,i,:])
+        if colors_f[n,i]>0:
+            eff_vectors_unit_f[n,i,:] = eff_vectors_f[n,i,:]/colors_f[n,i]
+            
+fig = plot_subplot_3well_effcurrent(eff_vectors_unit_f, colors_f, xn, yn, densAB,(xdim,ydim), (interval[0,0],interval[0,1],interval[1,0],interval[1,1]), N, (3*N,3),'Finite-time $f^+(n)$', subtitles=subtitles_f)
+fig.savefig(os.path.join(charts_path, 'triplewell_eff_f.png'), dpi=100)
