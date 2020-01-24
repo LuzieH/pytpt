c803a242f8f3d2b04253eba873034383a3160108
eborrell <ribera.borrell@me.com>
2020-01-24 16:46:35 +0100

    small network example: states 1 and 3 interchanged

--- tpt/small_network_construction.py
+++ tpt/small_network_construction.py
@@ -56,31 +56,29 @@ T[4, 4] = 0.7
 # L: 0-rowsum matrix
 # L+T does not have the 1-2 connection and L-T does not have the 2-3 connection 
 L = np.zeros((S, S))
-L[0, 1] = -0.05
-L[0, 3] =  0.05
-L[1, 0] =  0.2
-L[1, 2] = -0.4
-L[1, 4] =  0.2
-L[2, 1] = -0.2
-L[2, 3] =  0.2
-L[3, 0] = -0.2
-L[3, 2] =  0.4
-L[3, 4] = -0.2
-L[4, 1] = -0.05
-L[4, 3] =  0.05
+L[0, 1] =  0.05
+L[0, 3] = -0.05
+L[1, 0] = -0.2
+L[1, 2] =  0.4
+L[1, 4] = -0.2
+L[2, 1] =  0.2
+L[2, 3] = -0.2
+L[3, 0] =  0.2
+L[3, 2] = -0.4
+L[3, 4] =  0.2
+L[4, 1] =  0.05
+L[4, 3] = -0.05
 
 # K: 0-rowsum matrix
 # L+T+K transition matrix where A and B are less metastable
 K = np.zeros((S, S))
 K[0, 0] = -0.3
-K[0, 1] =  0.1
-K[0, 3] =  0.2
-K[1, 0] =  0.45
-K[1, 4] = -0.45
-#K[3,4] = -0.1
-#K[3,2] =  0.1
-K[4, 1] =  0.1
-K[4, 3] =  0.2
+K[0, 1] =  0.2
+K[0, 3] =  0.1
+K[3, 0] =  0.45
+K[3, 4] = -0.45
+K[4, 1] =  0.2
+K[4, 3] =  0.1
 K[4, 4] = -0.3
 
 np.save(os.path.join(my_path, 'data/small_network_states.npy'), states)
