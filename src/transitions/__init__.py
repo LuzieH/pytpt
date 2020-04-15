a00284d0cc4b62176042516b5f80741a8977b110
Luzie Helfmann <20030747+LuzieH@users.noreply.github.com>
2020-04-15 10:23:39 +0200

    update

--- src/transitions/__init__.py
+++ src/transitions/__init__.py
@@ -1,5 +1,4 @@
-from transitions import  transition_paths
-from transitions import  transition_paths_finite
-from transitions import  transition_paths_periodic
+from transitions import transition_paths
+from transitions import transition_paths_finite
+from transitions import transition_paths_periodic
 
-__all__ = ["transition_paths", "transition_paths_finite", "transition_paths_periodic"]
