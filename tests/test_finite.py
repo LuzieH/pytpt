03c3cd75c96f519908d3ea2ce462500f1bbcb7fb
eborrell <ribera.borrell@me.com>
2020-05-15 19:18:50 +0200

    #3 test small network from the examples (finite)

--- tests/test_finite.py
+++ tests/test_finite.py
@@ -2,19 +2,18 @@ from validation import is_stochastic_matrix, is_irreducible_matrix
 
 import numpy as np
 import pytest 
-from pytpt import finite
+from pytpt import finite, stationary
 import random
 import functools
 
 class TestFinite:
     @pytest.fixture
-    def P(self, S, N):
+    def P_random(self, S, N):
         ''' Random finite-time transition matrix
         Args:
         S: int 
             dimension of the state space 
         '''
- 
         # create random matrix uniformly distributed over [0, 1) and normalize
         # at time point mod 0
         P0 = np.random.rand(S, S)
@@ -22,61 +21,110 @@ class TestFinite:
         # at last time point 
         P1 = np.random.rand(S, S)
         P1 = np.divide(P1, np.sum(P1, axis=1).reshape(S, 1))
-        
         # transition matrix interpolates between P0 and P1 during time interval
         def P_N(k, N):
             gamma = k / (N-1) # ranges from 0 to 1 during time interval
             return (1 - gamma) * P0 + gamma * P1
-
         return functools.partial(P_N, N=N) 
     
     @pytest.fixture
-    def init_dens(self, S):
+    def init_dens_random(self, S):
         '''  
         Args:
         S: int 
             dimension of the state space 
         '''
- 
         init_dens = np.random.rand(S)
-        init_dens = init_dens/np.sum(init_dens)
-
+        init_dens = init_dens / np.sum(init_dens)
         return init_dens
     
     @pytest.fixture
-    def states(self, S):
+    def states_random(self, S):
         ''' States classification
         '''
         states = np.empty(S, dtype='str') 
-
         # sorted list of two elements chosen from the set of integers 
         # between 0 and S-1 without replacement
         i, j = sorted(random.sample(range(1, S), 2))
-
         states[:i] = 'A'
         states[i:j] = 'B'
         states[j:] = 'C'
+        return states
+    
+    @pytest.fixture
+    def P_small_network(self, shared_datadir):
+        ''' Finite-time transition matrix of the small network example
+        '''
+        small_network_construction = np.load(
+            shared_datadir / 'small_network_construction.npz',
+            allow_pickle=True, 
+        )
+        T = small_network_construction['T']
+        L = small_network_construction['L']
+        def P_hom(n):
+            return T + L
+
+        return P_hom
+    
+    @pytest.fixture
+    def init_dens_small_network(self, shared_datadir):
+        '''  
+        '''
+        small_network_construction = np.load(
+            shared_datadir / 'small_network_construction.npz',
+            allow_pickle=True, 
+        )
+        T = small_network_construction['T']
+        L = small_network_construction['L']
+        states = small_network_construction['states'].item()
         
+        ind_A = np.where(states == 'A')[0]
+        ind_B = np.where(states == 'B')[0]
+        ind_C = np.where(states == 'C')[0]
+
+        tpt_stationary = stationary.tpt(T + L, ind_A, ind_B, ind_C)
+        init_dens = tpt_stationary.stationary_density()
+        
+        return init_dens
+    
+    @pytest.fixture
+    def states_small_network(self, shared_datadir):
+        ''' States classification of the small network example
+        '''
+        small_network_construction = np.load(
+            shared_datadir / 'small_network_construction.npz',
+            allow_pickle=True, 
+        )
+        states = small_network_construction['states'].item()
         return states
 
     @pytest.fixture
-    def small_network_finite(self, states, P, init_dens, N):
+    def tpt_finite(self, N, states_random, P_random, init_dens_random, 
+                   states_small_network, P_small_network, init_dens_small_network, small_network):
         ''' initialize the tpt object 
         '''
-       
+        if small_network:
+            states = states_small_network
+            P = P_small_network
+            init_dens = init_dens_small_network
+        else:
+            states = states_random
+            P = P_random
+            init_dens = init_dens_random
+ 
         ind_A = np.where(states == 'A')[0]
         ind_B = np.where(states == 'B')[0]
         ind_C = np.where(states == 'C')[0]
         
-        small_network_finite = finite.tpt(P, N, ind_A, ind_B, ind_C, init_dens)
-        small_network_finite.committor()
+        tpt_finite = finite.tpt(P, N, ind_A, ind_B, ind_C, init_dens)
+        tpt_finite.committor()
         
-        return small_network_finite
+        return tpt_finite
 
-    def test_transition_matrix(self, small_network_finite):
-        S = small_network_finite._S
-        P = small_network_finite._P
-        N = small_network_finite._N
+    def test_transition_matrix(self, tpt_finite):
+        S = tpt_finite._S
+        P = tpt_finite._P
+        N = tpt_finite._N
         
         for n in range(N):
             assert P(n).shape == (S, S)
@@ -84,25 +132,25 @@ class TestFinite:
             assert is_stochastic_matrix(P(n))
             assert is_irreducible_matrix(P(n))
 
-    def test_density(self, small_network_finite):
-        S = small_network_finite._S
-        density = small_network_finite.density()
-        N = small_network_finite._N
-        P = small_network_finite._P
+    def test_density(self, tpt_finite):
+        S = tpt_finite._S
+        density = tpt_finite.density()
+        N = tpt_finite._N
+        P = tpt_finite._P
         
-        assert density.shape == (N,S)
+        assert density.shape == (N, S)
         assert np.isnan(density).any() == False
         assert np.greater_equal(density.all(), 0) 
         assert np.less_equal(density.all(), 1) 
         
-        for n in range(N-1):
-            assert np.isclose(density[n,:].dot(P(n)),density[n+1,:]).all()
+        for n in range(N - 1):
+            assert np.isclose(density[n, :].dot(P(n)), density[n + 1, :]).all()
             
 
-    def test_committors(self, small_network_finite):
-        q_f, q_b = small_network_finite._q_f, small_network_finite._q_b
-        S = small_network_finite._S
-        N = small_network_finite._N     
+    def test_committors(self, tpt_finite):
+        q_f, q_b = tpt_finite._q_f, tpt_finite._q_b
+        S = tpt_finite._S
+        N = tpt_finite._N     
 
         assert q_f.shape == (N,S)
         assert np.isnan(q_f).any() == False
@@ -114,32 +162,31 @@ class TestFinite:
         assert np.greater_equal(q_b.all(), 0) 
         assert np.less_equal(q_b.all(), 1) 
         
-    def test_reac_density(self, small_network_finite):
-        reac_dens = small_network_finite.reac_density()
-        norm_reac_dens = small_network_finite.norm_reac_density()
-        S = small_network_finite._S
-        N = small_network_finite._N  
+    def test_reac_density(self, tpt_finite):
+        reac_dens = tpt_finite.reac_density()
+        norm_reac_dens = tpt_finite.norm_reac_density()
+        S = tpt_finite._S
+        N = tpt_finite._N  
         
-        assert reac_dens.shape == (N,S)
+        assert reac_dens.shape == (N, S)
         assert np.isnan(reac_dens).any() == False
         assert np.greater_equal(reac_dens.all(), 0) 
         assert np.less_equal(reac_dens.all(), 1) 
         
-        assert norm_reac_dens.shape == (N,S)
+        assert norm_reac_dens.shape == (N, S)
         assert np.greater_equal(norm_reac_dens.all(), 0) 
         assert np.less_equal(norm_reac_dens.all(), 1)
         
  
-    def test_current(self, small_network_finite):
-        reac_current, eff_current = small_network_finite.reac_current()
-        S = small_network_finite._S
-        N = small_network_finite._N  
+    def test_current(self, tpt_finite):
+        reac_current, eff_current = tpt_finite.reac_current()
+        S = tpt_finite._S
+        N = tpt_finite._N  
         
-        assert reac_current.shape == (N,S,S)
+        assert reac_current.shape == (N, S, S)
         assert np.greater_equal(reac_current.all(), 0) 
         assert np.less_equal(reac_current.all(), 1) 
         
-        assert eff_current.shape == (N,S,S)
+        assert eff_current.shape == (N, S, S)
         assert np.greater_equal(eff_current.all(), 0) 
         assert np.less_equal(eff_current.all(), 1)
- 
